22517b944aebb17ad56f262b7a360d2e9ccb4b4d
Micael Levi L. Cavalcante <micalevisk@gmail.com>
2025-09-04 09:19:34 -0400

    refactor: remove unused module dep from `ShortUrlRestApiModule`

--- src/short-url/presentation/rest-api/short-url-rest-api.module.ts
+++ src/short-url/presentation/rest-api/short-url-rest-api.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { LoggerModule } from '../../../shared/infrastructure/logger/logger.module';
 import { ShortUrlModule } from '../../short-url.module';
 import { ResolveShortCodeController } from './controllers/resolve-short-code.controller';
 import { ShortenUrlController } from './controllers/shorten-url.controller';
 
 @Module({
-  imports: [LoggerModule, ShortUrlModule],
+  imports: [ShortUrlModule],
   controllers: [ShortenUrlController, ResolveShortCodeController],
 })
 export class ShortUrlRestApiModule {}
