eebd59b103d2d022588f49ecf5ca6a56829945dc
Micael Levi L. Cavalcante <micalevisk@gmail.com>
2025-09-16 15:45:55 -0400

    fix: move otel initialization to before everything

--- src/apps/rest-api/rest-api.main.ts
+++ src/apps/rest-api/rest-api.main.ts
@@ -1,16 +1,17 @@
+import { loadTracingIntoProcess } from '../../shared/infrastructure/telemetry/tracing-loader';
+
+loadTracingIntoProcess(); // NOTE: This must be called before everything else
+
 import { Logger as NestLogger, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import type { NestExpressApplication } from '@nestjs/platform-express';
 import { Logger } from 'nestjs-pino';
 
 import { environmentConfiguration } from '../../shared/infrastructure/environment.configuration';
-import { loadTracingIntoProcess } from '../../shared/infrastructure/telemetry/tracing-loader';
 import { loadSwaggerForNestjsApp } from '../../shared/presentation/rest-api/documentation/open-api-loader';
 import { RestApiAppModule } from './rest-api-app.module';
 
 async function bootstrap() {
-  loadTracingIntoProcess();
-
   const app = await NestFactory.create<NestExpressApplication>(
     RestApiAppModule,
     {
